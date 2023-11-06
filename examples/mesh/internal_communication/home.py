cf583e54addaa9b70e359c71ed24028c46aa6167
mr fok <fuck@you>
2023-11-06 12:32:01 +0200

    home improvements

--- examples/mesh/internal_communication/home.py
+++ examples/mesh/internal_communication/home.py
@@ -4,16 +4,12 @@ from construct import *
 import serial
 
 def get_parser() -> argparse.ArgumentParser:
-#{
     parser = argparse.ArgumentParser()
     parser.add_argument('port', )
     return parser
-#}
 
 class Commander:
-#{
     def __init__(self, shit, baudrate):
-    #{
         self.s = serial.Serial(port=shit, baudrate=baudrate)
         self.format = Struct(
             "cmd" / Enum(Int32ul, 
@@ -25,22 +21,24 @@ class Commander:
             "len" / Short,
             "buf" / Array(this.len, Byte)
         )
-    #}
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.s.close()
+
+    def close(self):
+        self.s.close()
 
     def send_become_root(self):
-    #{
         b = self.format.build(dict(cmd=self.format.cmd.BECOME_ROOT, len=0, buf=b""))
         print(b)
         self.s.write(b)
-    #}
 
     def send_start_keep_alive(self):
-    #{
         b = self.format.build(dict(cmd=self.format.cmd.START_KEEP_ALIVE, len=0, buf=b""))
         print(b)
         self.s.write(b)
-    #}
-#}
 
 
 # if __name__ == '__main__':
